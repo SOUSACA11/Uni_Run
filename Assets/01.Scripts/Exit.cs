cb7247ad64a46c5e414c6168db372151e7138cdb
JINNY <sousaca05@gmail.com>
2023-05-30 12:38:06 +0900

    230530
    유니런 완

--- Assets/01.Scripts/Exit.cs
+++ Assets/01.Scripts/Exit.cs
@@ -1,6 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+
+
+  //버튼 컴포넌트는 이미지를 인식하는 크기 범위
+  //이미지에 의해서 인식
 
 public class Exit : MonoBehaviour
 {
@@ -9,4 +12,29 @@ public class Exit : MonoBehaviour
         Debug.Log("게임 종료");
         Application.Quit();
     }
+
+    //public void ClickRestart()
+    //{
+    //    Debug.Log("게임 재시작");
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    //    //on click - Time.timeScale = 1;
+    //}
+
+
+    // Time.timeScale은 극단적 상황
+
+    //public void TTime(int time) //게임의 시간 흐름 조정 기능
+    //{
+    //    Debug.Log("일시정지");
+    //    Time.timeScale = time; //기본값 1 - 시간 흐름 / 0 - 시간정지
+    //}
 }
+
+
+// 일시정지 때 바탕화면 클릭시 게임 이어하기
+// 패널 컴포넌트 안에 버튼 추가
+// on click - 자기 자신 컴포넌트 드래그 앤 드롭 - Gameobject.setactive 비활성화
+// on click - Time.timeScale = 1;
+
+
+// interactable = 상호작용 - 이미지 활성화 비활성화 부분
